bd612bb39803a7d34f2b3599cf2e4c2c77b307d3
JunhoYeo <32605822+JunhoYeo@users.noreply.github.com>
2019-09-10 14:01:23 +0900

    refactor: auth middleware with promise

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,17 +1,42 @@
-import { sign as signJwt, verify as verifyJwt } from 'jsonwebtoken';
+import * as jwt from 'jsonwebtoken';
 import { randomBytes } from 'crypto';
 
 const secret = randomBytes(10).toString('base64');
 
-export function sign(data) {
-  return signJwt({ data }, secret);
+const sign = (data) => {
+  return jwt.sign({ data }, secret);
 }
 
-export function verify(token) {
-  return verifyJwt(token, secret, (err, result) => {
-    if (err) {
-      return err;
-    }
-    return result;
-  });
+const verify = (token) => new Promise(
+  (resolve, reject) => {
+    jwt.verify(token, secret, (err, decoded) => {
+      if (err) reject(err)
+      resolve(decoded)
+    })
+  }
+);
+
+const authMiddleware = (req, res, next) => {
+  const token = req.headers['Authorization'] || req.query.token
+  if (!token) {
+    return res.status(401).json({
+      message: 'Unauthorized; No token found'
+    })
+  }
+
+  try {
+    const { decoded } = await verify(token);
+    req.decoded = decoded;
+    next()
+  } catch (error) {
+    res.status(403).json({
+      success: false,
+      message: error.message
+    })
+  }
 }
+
+module.exports = {
+  sign,
+  authMiddleware,
+};

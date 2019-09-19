c58cc0ff0e376eb81ad1fe854e2aa75c2967c01a
JunhoYeo <32605822+JunhoYeo@users.noreply.github.com>
2019-09-20 03:22:15 +0900

    feat(log): dashboard api

--- middleware/logger.js
+++ middleware/logger.js
@@ -1,13 +1,15 @@
 import asyncHandler from 'express-async-handler';
 import Log from '../models/log';
 
-export const loggerMiddleware = asyncHandler(async (req, res, next) => {                                                                                                                   
+export const loggerMiddleware = asyncHandler(async (req, res, next) => {
+  if (req.originalUrl === '/log')
+    return next();
   const startHrTime = process.hrtime();
 
   res.on("finish", () => {
     const elapsedHrTime = process.hrtime(startHrTime);
     const elapsedTimeInMs = elapsedHrTime[0] * 1000 + elapsedHrTime[1] / 1e6;
-    Log.create(req.path, req.method, res.statusCode, elapsedTimeInMs);
+    Log.create(req.originalUrl, req.method, res.statusCode, elapsedTimeInMs);
   });
   return next();
 });
--- router/log/index.js
+++ router/log/index.js
@@ -5,15 +5,21 @@ import Log from '../../models/log';
 const router = Router();
 
 router.get('/', asyncHandler(async (req, res, _) => {
+  const today = new Date().getTime();
   let yesterday = new Date();
-  yesterday.setDate(yesterday.getDate() - 1);
+  yesterday.setDate(yesterday.getDate() - 1)
+  yesterday = yesterday.getTime();
   const logs = await Log.find({
     'timestamp': {
-      "$gt": yesterday.getTime(),
-      "$lt": new Date().getTime(),
+      "$gt": yesterday,
+      "$lt": today,
     },
   });
-  return res.json(logs);
+  return res.json({
+    start: yesterday,
+    end: today,
+    logs,
+  });
 }));
 
 export default router;

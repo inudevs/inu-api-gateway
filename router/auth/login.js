31f8e610261d5cdcd53229dbf8f91ea92f501420
JunhoYeo <32605822+JunhoYeo@users.noreply.github.com>
2019-09-20 18:05:22 +0900

    feat: login with dimigo account

--- router/auth/login.js
+++ router/auth/login.js
@@ -8,13 +8,24 @@ const router = Router();
 // POST /auth/login
 router.post('/', asyncHandler(async (req, res, _) => {
   const { email, password } = req.body;
-  const user = await User.findOne({ email });
+  let user = {};
+  if (!email.includes('@')) { // 이메일이 아님
+    // 디미고 아이디니까 그걸로 디비 쿼리
+    user = await User.findOne({ 'student.id': email });
+    if (!user) {
+      return res.status(404).json({
+        message: '주어진 디미고 아이디를 가진 사용자가 없습니다.',
+      });
+    }
+  } else { // 이메일
+    user = await User.findOne({ email });
 
-  // 해당 이메일의 사용자가 존재하지 않음
-  if (!user) {
-    return res.status(404).json({
-      message: '주어진 이메일을 가진 사용자가 없습니다.',
-    });
+    // 해당 이메일의 사용자가 존재하지 않음
+    if (!user) {
+      return res.status(404).json({
+        message: '주어진 이메일을 가진 사용자가 없습니다.',
+      });
+    }
   }
 
   // 잘못된 패스워드

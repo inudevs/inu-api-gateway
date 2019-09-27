994ba196cc8146bbefa344ece0f093744893f1d6
JunhoYeo <32605822+JunhoYeo@users.noreply.github.com>
2019-09-27 23:32:48 +0900

    fix(service): default proxy path to request

--- middleware/proxy.js
+++ middleware/proxy.js
@@ -5,7 +5,7 @@ import Service from '../models/service';
 
 export const proxyMiddleware = proxy(async (req) => {
   const { params: { route } } = req;
-  const path = `/${req.params[0] || ''}`;
+  const path = req.params[0] || '';
 
   const service = await Service.findOne({ route, proxy: true });
   return url.resolve(service.api, path);
